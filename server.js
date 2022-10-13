c98b42452e90cf28aac67a14a95311ba6652693c
Patrick  Lin <58764316+asdfgh31100606@users.noreply.github.com>
2022-10-14 01:07:38 +0800

    ver_1.0

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.set('view-engine')
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+app.get('/page2', (req, res) => {
+    res.render('page2.ejs')
+})
+app.get('/result', (req, res) => {
+    res.render('result_page.ejs')
+})
+
+app.listen(port, () => {
+    console.log(`radarchart listening on port ${port}`)
+})
